f751f8d1e147151597215af928b4f455fbdb1041
Lakshay Raturi <85173668+lakshayraturi@users.noreply.github.com>
2022-04-17 22:24:56 +0530

    Init

---
+++ lib/constant.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const kBackgroundColor = Color(0xFFFEFEFE);
+const kTitleTextColor = Color(0xFF303030);
+const kBodyTextColor = Color(0xFF4B4B4B);
+const kTextLighColor = Color(0xFF959595);
+const kInfectedColor = Color(0xFFFF8748);
+const kDeathColor = Color(0xFFFF4848);
+const kRecoverColor = Color(0xFF36C12C);
+const kPrimaryColor = Color(0xFF3382CC);
+final kShadowColor = const Color(0xFFB7B7B7).withOpacity(0.16);
+final kActiveShadowColor = const Color(0xFF4056C6).withOpacity(0.15);
+
+const kHeadingTextStyle = TextStyle(
+  fontSize: 22,
+  fontWeight: FontWeight.w600,
+);
+
+const kSubTextStyle = TextStyle(
+  fontSize: 16,
+  color: kTextLighColor,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 18,
+  color: kTitleTextColor,
+  fontWeight: FontWeight.bold,
+);
---
+++ lib/main.dart
@@ -0,0 +1,130 @@
+import 'package:flutter/material.dart';
+import 'package:covid_19/constant.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Covid 19',
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        fontFamily: "poppins",
+        textTheme: const TextTheme(
+            bodyText1: TextStyle(
+          color: kBodyTextColor,
+        )),
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          ClipPath(
+            clipper: MyClipper(),
+            child: Container(
+              padding: const EdgeInsets.only(
+                left: 40,
+                top: 50,
+                right: 20,
+              ),
+              height: 350,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topRight,
+                  end: Alignment.bottomLeft,
+                  colors: [
+                    Color(0xFF3383CD),
+                    Color(0xFF11249F),
+                  ],
+                ),
+                image: DecorationImage(
+                  image: AssetImage("assets/images/virus.png"),
+                ),
+              ),
+              child: Column(
+                children: [
+                  Align(
+                    alignment: Alignment.topRight,
+                    child: SvgPicture.asset("assets/icons/menu.svg"),
+                  ),
+                  const SizedBox(height: 20),
+                  Expanded(
+                    child: Stack(
+                      children: [
+                        SvgPicture.asset(
+                          "assets/icons/Drorona.svg",
+                          width: 230,
+                          fit: BoxFit.fitWidth,
+                          alignment: Alignment.topCenter,
+                        ),
+                        Positioned(
+                          top: 20,
+                          left: 150,
+                          child: Text(
+                            "All you need \nis stay at home.",
+                            style: kHeadingTextStyle.copyWith(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                        Container(),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            height: 60,
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(25),
+              border: Border.all(
+                color: const Color(0xFFE5E5E5),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class MyClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height - 80);
+    path.quadraticBezierTo(
+        size.width / 2, size.height, size.width, size.height - 80);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
